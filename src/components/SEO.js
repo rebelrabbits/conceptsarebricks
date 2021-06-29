f2ae8a0640eb5a9d697bbd2c24bbe4e94ab757ec
rebelrabbits <alexmiller@alexs-mbp.home>
2021-06-29 17:45:44 -0400

    using example from https://www.gatsbyjs.com/docs/add-seo-component/ docs

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
+import { useLocation } from "@reach/router";
 
 const fetchMetaData = graphql`
   query {
     site {
       siteMetadata {
-        siteTitle: title
-        siteDesc: description
-        author
+        defaultTitle: title
+        defaultDescription: description
         siteUrl
-        image
+        defaultImage: image
         twitterUsername
       }
     }
@@ -23,28 +23,57 @@ const Seo = ({
   featuredImage = null,
   author = null,
   isFullImage = true,
-  path = "",
 }) => {
   const { site } = useStaticQuery(fetchMetaData);
+  const { pathname } = useLocation();
+
+  console.log("pathname", pathname);
+
   const {
-    siteDesc,
-    siteTitle,
+    defaultDescription,
+    defaultTitle,
     siteUrl,
-    image,
+    defaultImage,
     twitterUsername,
   } = site.siteMetadata;
-  const imageURL = `${siteUrl}${featuredImage ? featuredImage : image}`;
-  const seoTitle = title ? `${title} | ${siteTitle}` : siteTitle;
-  const seoDescription = description ? description : siteDesc;
-  const url = `https://www.conceptsarebricks.com${path}`;
-  console.log("URL passed: ", url);
-  console.log("siteUrl: ", siteUrl);
+  // const imageURL = `${siteUrl}${featuredImage ? featuredImage : defaultImage}`;
+  // const seoTitle = title ? `${title} | ${defaultTitle}` : defaultTitle;
+  // const seoDescription = description ? description : defaultDescription;
+  // const url = `${siteUrl}${pathname}`;
+  // console.log("URL passed: ", url);
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: `${siteUrl}${featuredImage || defaultImage}`,
+    url: `${siteUrl}${pathname}`,
+  };
+
+  console.log("seo: ", seo);
 
   return (
-    <Helmet title={seoTitle} htmlAttributes={{ lang: "en" }}>
-      <meta name='description' content={seoDescription} />
-      {/* OG */}
-      <meta property='og:title' content={seoTitle} />
+    <Helmet title={seo.title} htmlAttributes={{ lang: "en" }}>
+      <meta name='description' content={seo.description} />
+      <meta name='image' content={seo.image} />
+      {seo.url && <meta property='og:url' content={seo.url} />}
+      <meta property='og:type' content='article' />
+      {seo.title && <meta property='og:title' content={seo.title} />}
+      {seo.description && (
+        <meta property='og:description' content={seo.description} />
+      )}
+      {seo.image && <meta property='og:image' content={seo.image} />}
+      <meta name='twitter:card' content='summary_large_image' />
+      {twitterUsername && (
+        <meta name='twitter:creator' content={twitterUsername} />
+      )}
+      {seo.title && <meta name='twitter:title' content={seo.title} />}
+      {seo.description && (
+        <meta name='twitter:description' content={seo.description} />
+      )}
+      {seo.image && <meta name='twitter:image' content={seo.image} />}
+      {/* <meta name='description' content={seoDescription} />
+
+<meta property='og:title' content={seoTitle} />
       <meta property='og:description' content={seoDescription} />
       <meta property='og:type' content='article' />
       <meta property='og:url' content={url} />
@@ -53,8 +82,8 @@ const Seo = ({
       <meta property='og:image:width' content='1144' />
       <meta property='og:image:height' content='766' />
       <meta name='author' content={author || "Rebel Rabbits"}></meta>
-      {/* Twitter Card */}
-      <meta
+
+<meta
         name='twitter:card'
         content={isFullImage ? "summary_large_image" : "summary"}
       />
@@ -62,7 +91,7 @@ const Seo = ({
       <meta name='twitter:title' content={seoTitle} />
       <meta name='twitter:description' content={seoDescription} />
       <meta name='twitter:image' content={imageURL} />
-      <link rel='canonical' href={url}></link>
+      <link rel='canonical' href={url}></link> */}
     </Helmet>
   );
 };
